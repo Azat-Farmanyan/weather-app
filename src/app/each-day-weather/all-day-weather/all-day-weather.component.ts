a0876f479b34427bc847cce42eb8389d2fdb9541
Azat-Farmanyan <azat.farmanyan2@mail.ru>
2022-12-26 11:21:25 +0400

    feat: last changes

--- src/app/each-day-weather/all-day-weather/all-day-weather.component.ts
+++ src/app/each-day-weather/all-day-weather/all-day-weather.component.ts
@@ -44,7 +44,7 @@ export class AllDayWeatherComponent implements OnInit, OnDestroy {
                   });
                   this.isLoading = false;
 
-                  // console.log(this.allDayWeather);
+                  console.log(this.allDayWeather);
                 });
               },
               (error) => {
